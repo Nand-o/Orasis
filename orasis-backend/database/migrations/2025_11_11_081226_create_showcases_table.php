b08c9b5f51bac4eaf3a82a92b4dc6a18cba8b7d9
bai <farismaulanaalbai@gmail.com>
2025-11-12 11:21:09 +0700

    adding new field into showcase_table

--- orasis-backend/database/migrations/2025_11_11_081226_create_showcases_table.php
+++ orasis-backend/database/migrations/2025_11_11_081226_create_showcases_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->string('title');
+            $table->string('url_website');
             $table->text('description')->nullable();
             $table->string('image_url');
             $table->string('category')->nullable();
--- orasis-backend/database/seeders/ShowcaseSeeder.php
+++ orasis-backend/database/seeders/ShowcaseSeeder.php
@@ -13,10 +13,12 @@ class ShowcaseSeeder extends Seeder
      */
     public function run(): void
     {
+        // ADD URL 
         $showcases = [
             [
                 'user_id' => 1,
                 'title' => 'Minimalist Portfolio Website',
+                'url_website' => 'https://dribbble.com/shots/portfolio-minimalist',
                 'description' => 'A clean and modern portfolio layout for designers.',
                 'image_url' => 'https://placehold.co/600x400?text=Portfolio+Web',
                 'category' => 'Web Design',
@@ -24,6 +26,7 @@ class ShowcaseSeeder extends Seeder
             [
                 'user_id' => 2,
                 'title' => 'Mobile App Dashboard UI',
+                'url_website' => 'https://dribbble.com/shots/mobile-dashboard-ui',
                 'description' => 'Dashboard interface for mobile productivity apps.',
                 'image_url' => 'https://placehold.co/600x400?text=Dashboard+UI',
                 'category' => 'App Design',
@@ -31,6 +34,7 @@ class ShowcaseSeeder extends Seeder
             [
                 'user_id' => 3,
                 'title' => 'Creative Poster Design',
+                'url_website' => 'https://behance.net/gallery/creative-poster-design',
                 'description' => 'Poster concept for an art exhibition with bold typography.',
                 'image_url' => 'https://placehold.co/600x400?text=Poster+Design',
                 'category' => 'Graphic Design',
