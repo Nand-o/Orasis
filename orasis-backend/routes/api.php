da9e43b8be10fe8d1b050f903ed50ba998c2449c
CanLikez <candraafriansyah228@gmail.com>
2025-11-15 02:25:49 +0700

    implement sanctum based admin login, add admin middleware protection, add personal access token table

--- orasis-backend/routes/api.php
+++ orasis-backend/routes/api.php
@@ -5,19 +5,36 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ShowcaseController;
 use App\Http\Controllers\TagController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\AuthController;
 
-// fitur buat user auth nanti
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// public route
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+// protected route
+Route::middleware('auth:sanctum')->group(function () {
+    
+    // endpoint untuk mengambil data user yang sedang login
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    // endpoint logout
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    // admin route
+    // Rute ini harus login and admin
+    Route::middleware('is.admin')->group(function () {
+
+    // endpoint showcase
+    Route::apiResource('showcases', ShowcaseController::class);
+
+    // endpoint untuk Tag
+    Route::apiResource('tags', TagController::class);
+
+    // endpoint user
+    Route::apiResource('users', UserController::class);
+    });
 });
 
-// CRUD ADMIN FITUR
 
-// endpoint showcase
-Route::apiResource('showcases', ShowcaseController::class);
-
-// endpoint untuk Tag
-Route::apiResource('tags', TagController::class);
-
-// endpoint user
-Route::apiResource('users', UserController::class);
